2b5c25686022f7fedfe75bbc96cb1a5bffcef228
baovkb <21521860@gm.uit.edu.vn>
2024-11-20 20:22:43 +0700

    refactor

--- MMM-Screen-Control.js
+++ MMM-Screen-Control.js
@@ -1,263 +1,184 @@
 Module.register("MMM-Screen-Control", {
-	isHiding: false,
-	modules: [],
-	locking: false,
-	animationTime: 500,
-	currentPage: 0,
-	totalPage: 0,
+    isLocking: false,
+    profileData: null,
+    indexProfile: 0,
 
 	defaults: {
+		profiles: [],
+        animationTime: 500,
 	},
 
-	start: function(){
+	init: function() {
 	},
+
+	start: function() {
+        
+	},
+
+	getScripts: function() {
+	    return	[]
+	}, 
+
+	getStyles: function() {
+		return ['style.css']
+	},
+
+    initData: function () {
+        this.profileData = Array(this.config.profiles.length).fill(null).map(() => ({}));
+
+        this.profileData.forEach((profile, indexProfile) => {            
+            profile.name = this.config.profiles[indexProfile].name
+            profile.currentPage = 0
+            
+            profile.pages = this.config.profiles[indexProfile].pages.map(page => 
+                page.map((className) => {
+                    module = MM.getModules().find(module => module.data.classes.includes(className))
+                    return {
+                        name: className,
+                        identifier: module.identifier,
+                        hidden: false
+                    }
+            }
+            ))
+            
+        })
+    },
 
 	notificationReceived: function(notification, payload, sender) {
-		if(!sender && notification=== 'ALL_MODULES_STARTED'){
-			this.sendSocketNotification("CONFIG",this.config);	
-			this.setAnimationTime();
-		}
-		else {
-			if (notification === "MMM-Screen-Control") {
-				if (payload.type === "HIDE_SCREEN") {
-					this.hideScreen();
-				} else if (payload.type === "DISPLAY_SCREEN") {
-					this.displayScreen();
-				} else if (payload.type === "HIDE_SCREEN_SCHEDULE") {
-					
-				}else if (payload.type === "DISPLAY_SCREEN_SCHEDULE") {
-					
-				} else if (payload.type === "CHANGE_PAGE" && this.isHiding === false) {
-					if (payload.cmd !== undefined) {
-						this.changePage(payload.cmd);
-					}
-				} else if (payload.type === "CHANGE_MODULES") {
-					data = payload.data;
-					modulesRequest = data["modules"];
+        switch (notification) {
+            case "ALL_MODULES_STARTED":
+                this.initData()
+                break
+            case "DOM_OBJECTS_CREATED":
+                this.updatePageDom()
+                break
+            case "MMM-Screen-Control": 
+                currentPage = this.profileData[this.indexProfile].currentPage
+                pageLength = this.profileData[this.indexProfile].pages.length
+
+                if (payload.type == "NEXT_PAGE") {
+                    tmp = currentPage == pageLength - 1 ? 0 : currentPage + 1
+                    this.selectPage(tmp)
+                } else if (payload.type == "PREVIOUS_PAGE") {
+                    tmp = currentPage == 0 ? pageLength - 1 : currentPage - 1
+                    this.selectPage(tmp)
+                }
+                else if (payload.type == "CHANGE_MODULES") {
+                    modulesByPage = this.profileData[this.indexProfile].pages[currentPage]
+                    modulesRequest = payload.data;
+			
 					if (!Array.isArray(modulesRequest)) {
 						modulesRequest = Array.from(modulesRequest);
 					}
-					
-					hide_identifers = [];
-					display_identifers = [];
-					
-					modulesRequest.forEach((module) => {
-						if (module.hidden === true) {
-							hide_identifers.push(module.identifier);
-						} else {
-							display_identifers.push(module.identifier);
-						}
-					});
-					
-					this.hideModule(hide_identifers, data["page"]);
-					this.displayModule(display_identifers, data["page"]);
-							
-					this.sendNotification("MMM-WS-Control",{
-						type: "MODULES_UPDATED",
-						page: data["page"],
-						total_page: this.totalPage,
-						page_modules: this.modules[data["page"]]
-					});	
 
-				} else {
-					
-				}
-			} else if (notification == "EXT_PAGES-NUMBER_IS") {
-				this.currentPage = payload.Actual;
-				this.totalPage = payload.Total;
-				
-				this.handleModulesByPage();
-					
-				this.sendNotification("MMM-WS-Control",{
-					type: "PAGE_CHANGED",
-					page: this.currentPage,
-					total_page: this.totalPage,
-					page_modules: this.modules[this.currentPage]
-				});	
-			
-			} else if (notification == "DOM_OBJECTS_CREATED") {
-				this.getAllVisibleModule();
-				this.sendNotification("EXT_PAGES-NUMBER");
-			}
-		}
+                    modulesByPage.forEach(module => {
+                        foundModule = modulesRequest.find(m => m.identifier == module.identifier);
+                        if (foundModule != null) {
+                            module.hidden = foundModule.hidden
+                        }
+                    })
+
+                    this.updatePageDom()
+                } else if (payload.type == "CHANGE_PROFILE") {
+                    firstInd = this.profileData.findIndex(profile => profile.name == payload.data)
+                    if (firstInd == -1) return;
+
+                    this.indexProfile = firstInd;
+                    this.updateDom()
+                    this.updatePageDom()
+
+                } else {
+
+                }
+            default: break
+
+        }
 	},
 
 	socketNotificationReceived: function(notification, payload) {
-		Log.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
-		if(notification === "message_from_helper"){
-			this.config.message = payload;
-		}
+	},
+
+	suspend: function(){
 
 	},
 
-	hideScreen: function() {
-		//vcgencmd display_power 0 
-		if (this.isHiding == false && this.locking === false) {
-			MM.getModules().forEach(module => {
-				module.hide(this.animationTime, { lockString: "MMM-Screen-Control-lock-all" });
-			});
-			
-			this.isHiding = true;
-			this.locking = true;
-			
-			setTimeout(() => {
-				this.locking = false;
-			}, this.animationTime);
-		}
+	resume: function(){
+
 	},
 
-	displayScreen: function() {
-		//vcgencmd display_power 1
-		if (this.isHiding == true && this.locking === false) {
-			MM.getModules().forEach(module => {
-				module.show(this.animationTime, { lockString: "MMM-Screen-Control-lock-all" });
-			});
-			
-			this.isHiding = false;
-			this.locking = true;
-			
-			setTimeout(() => {
-				this.locking = false;
-			}, this.animationTime);
-		}
+	getDom: function() {
+        self = this
+		var wrapper = document.createElement("div")
+        wrapper.classList.add('container')
+        wrapper.classList.add('mmm-screen-control')
+
+        currentProfile = this.profileData[this.indexProfile]
+
+        for (let i = 0; i < currentProfile.pages.length; ++i) {
+            //build indicator
+            const circle = document.createElement('div');
+            circle.classList.add('circle');
+            circle.classList.add('mmm-screen-control')
+            if (i == this.profileData[this.indexProfile].currentPage) {
+                circle.classList.add('active');
+            }
+
+            circle.addEventListener('click', () => {
+                if (this.isLocking == true || i == this.profileData[this.indexProfile].currentPage) return;
+
+                self.selectPage(i)
+            });
+
+            wrapper.appendChild(circle);
+        }
+
+		return wrapper;
 	},
 
-	hideModule: function(identifiers, page) {
-		if (!Array.isArray(identifiers)) {
-			identifiers = Array.from(identifiers);
-		}
-		
-		
-		MM.getModules().forEach(module => {
-			existInModules = this.moduleInPage(module.identifier, page);
-			if (identifiers.includes(module.identifier) && existInModules !== -1) {
-				//if (module.hidden == false) {
-					module.hide(this.animationTime, { lockString: "MMM-Screen-Control" });
-					this.modules[page][existInModules].hidden = true;
-				//}
-				
-			}
-		});
-	},
-	
-	moduleInPage: function(identifier, page) {
-		for (ind in this.modules[page]) {
-			if (this.modules[page][ind].identifier === identifier) {
-				return ind;
-			}
-		}
-		
-		return -1;
-	},
+    selectPage: function(newPage) {
+        let circles = document.querySelectorAll('.circle.mmm-screen-control');
+        circles.forEach(c => c.classList.remove('active'))
+        circles[newPage].classList.add('active');
 
-	displayModule: function(identifiers, page) {		
-		if (!Array.isArray(identifiers)) {
-			identifiers = Array.from(identifiers);
-		}
-		
-		MM.getModules().forEach(module => {
-			existInModules = this.moduleInPage(module.identifier, page);
-			if (identifiers.includes(module.identifier) && existInModules !== -1) {
-				//if (module.hidden == true) {
-					module.show(this.animationTime, { lockString: "MMM-Screen-Control" });
-					this.modules[page][existInModules].hidden = false;
-				//}
-				
-			}
-		});
-	},
-	
-	changePage: function(cmd) {
-		console.log("change page: " + cmd);
-		if (this.locking === false) {
-			this.locking = true;
-			this.sendNotification(cmd);
-			
-			setTimeout(() => {
-				
-				this.locking = false;
-			}, this.animationTime);
-		}
-	},
-	
-	handleModulesByPage: function() {
-		identiferArr = this.modules[this.currentPage].map((module) => module.identifier);
-		console.log(this.modules);
-		
-		hide_identifers = [];
-		display_identifers = [];
-		MM.getModules().forEach(module => {
-			desInd = identiferArr.indexOf(module.identifier);
-			if (desInd !== -1) {
-				if (this.modules[this.currentPage][desInd].hidden === true){
-					hide_identifers.push(this.modules[this.currentPage][desInd].identifier);
-				} else {
-					display_identifers.push(this.modules[this.currentPage][desInd].identifier);
-				}
-			}
-		});
-		
-		console.log("cac module se bi an");
-		console.log(hide_identifers);
-		console.log("cac module se hien thi");
-		console.log(display_identifers);
-		
-		this.hideModule(hide_identifers, this.currentPage);
-		this.displayModule(display_identifers, this.currentPage);
-	},
-	
-	setAnimationTime: function() {
-		MM.getModules().forEach(module => {
-			if (module.name == "EXT-Pages") {
-				this.animationTime = module.config.animationTime;
-				return;
-			}
-		});
-	},
-	
-	getAllVisibleModule: function() {
-		this.modules = [];
-  
-		MM.getModules().forEach(module => {
-			if (module.name === "EXT-Pages") {
-				pagesObj = module.config.pages;
-				extPageFixed = module.config.fixed;
-				
-				pagesArr = Object.values(pagesObj);
-				
-				pagesArr.forEach(page => {
-					classStr = page.join(' ') + ' ' + extPageFixed.join(' ');
-					classStr = classStr.trim();
-        
-					tmp = [];
-					MM.getModules().forEach(module => {
-						let classList = module.data.classes.split(' ');
-						classList.forEach(cls => {
-							if (classStr.includes(cls)) {
-								let name = this.modifyName(module.data.name);				
-								tmp.push({
-									"name": name,
-									"hidden": false,
-									"identifier": module.identifier,
-								});
-							}	
-						});
-    
-					});
-					this.modules.push(tmp);
-        
-				});		
-				return;
-			}
-		});
-	},
+        this.profileData[this.indexProfile].currentPage = newPage;
 
-	modifyName: function(name) {
-		name = name.replace("MMM", '');
-		name = name.replace('-', ' ');
-		name = name.trim();
+        this.updatePageDom();
+    },
 
-		return name;
-	},
-	
+    updatePageDom: function() {
+        this.isLocking = true;
+        displayModules = []
+
+        MM.getModules().forEach(module => {
+            page = this.profileData[this.indexProfile].currentPage
+            currentPage = this.profileData[this.indexProfile].pages[page]
+
+            currentPage.forEach(pageModule => {
+                if (module.identifier == pageModule.identifier && pageModule.hidden == false) 
+                    displayModules.push(module.identifier)
+            })
+        })
+
+        MM.getModules().forEach(module => {
+            if (module.data.classes.includes(this.data.classes)) return;
+            let found = displayModules.findIndex(m => m == module.identifier);
+            if (found != -1) {
+                module.show(this.config.animationTime, () => null, { lockString: "MMM-Screen-Control" })
+            } else module.hide(this.config.animationTime, () => null, { lockString: "MMM-Screen-Control" })
+            
+        })
+
+        setTimeout(() => {
+            this.isLocking = false;
+        }, this.animationTime);
+
+        currentPage = this.profileData[this.indexProfile].currentPage
+
+        this.sendNotification('PAGE_CHANGED', {
+            profile: this.profileData[this.indexProfile].name,
+            currentPage: currentPage,
+            totalPage: this.profileData[this.indexProfile].pages.length,
+            pageModules: this.profileData[this.indexProfile].pages[currentPage]
+        })
+    },
 })
